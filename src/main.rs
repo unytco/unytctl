54394bfb3b43e072bfc2a4423d73ee3e1766a0ef
ThetaSinner <ThetaSinner@users.noreply.github.com>
2026-08-12 12:48:31 +0100

    feat: Add command for bootstrapping the joining service

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use clap::{Parser, Subcommand};
 use hc_seed_bundle::dependencies::sodoken::LockedArray;
 use std::io::Write;
+use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
+use hc_seed_bundle::SharedLockedArray;
 use zeroize::Zeroizing;
 
 #[derive(Parser, Debug)]
@@ -15,6 +17,16 @@ pub enum CliCommand {
     Progenitor {
         #[arg(long)]
         passphrase: Option<String>,
+    },
+    MembraneProof {
+        #[clap(long)]
+        seed_bundle: PathBuf,
+
+        #[clap(long)]
+        seed_bundle_passphrase: Option<String>,
+
+        #[clap(long)]
+        joining_service_url: String,
     }
 }
 
@@ -24,22 +36,38 @@ async fn main() -> anyhow::Result<()> {
 
     match cli.command {
         CliCommand::Progenitor { passphrase } => {
-            let pass = match passphrase {
-                Some(passphrase) => passphrase,
-                None => {
-                    rpassword::prompt_password("Enter passphrase: ")?
-                }
-            };
-            let pass = Zeroizing::new(pass);
-            let pass_locked = LockedArray::from(pass.as_bytes().to_vec());
+            let pass_locked = read_passphrase(passphrase)?;
 
-            let bundle = unytctl::create_progenitor(Arc::new(Mutex::new(pass_locked))).await?;
+            let bundle = unytctl::create_progenitor(pass_locked).await?;
 
             let mut out = std::io::stdout();
             out.write_all(bundle.to_json()?.as_bytes())?;
             out.flush()?;
         }
+        CliCommand::MembraneProof { seed_bundle, seed_bundle_passphrase, joining_service_url } => {
+            let pass_locked = read_passphrase(seed_bundle_passphrase)?;
+
+            let seed_bundle = std::fs::read_to_string(seed_bundle)?;
+
+            let proofs = unytctl::create_membrane_proof(joining_service_url, seed_bundle, pass_locked).await?;
+
+            let mut out = std::io::stdout();
+            out.write_all(proofs.to_json()?.as_bytes())?;
+            out.flush()?;
+        }
     };
 
     Ok(())
+}
+
+fn read_passphrase(passphrase: Option<String>) -> Result<SharedLockedArray, std::io::Error> {
+    let pass = match passphrase {
+        Some(passphrase) => passphrase,
+        None => {
+            rpassword::prompt_password("Enter passphrase: ")?
+        }
+    };
+    let pass = Zeroizing::new(pass);
+    let pass_locked = LockedArray::from(pass.as_bytes().to_vec());
+    Ok(Arc::new(Mutex::new(pass_locked)))
 }
